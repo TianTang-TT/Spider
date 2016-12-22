3ec6402b08546685a589fdbbc54db3c564f5077c
Honor <tiantang.honor@gmail.com>
2016-12-22 13:24:32 +0800

    update

--- crawlers/zhihu.js
+++ crawlers/zhihu.js
@@ -8,15 +8,16 @@ const actions = []; // 最近动态，取两百条，然后分析
 const actionSize = 20;   // 每次请求的数目
 const actionCount = 200; // 需要的数据总数
 // 知乎个人中心地址
-const profileUrl = 'https://www.zhihu.com/people/yu-xin/activities';
-// 此接口请求的最大条数为 20
+
+let userId = 'yu-xin-96-75';
+const profileUrl = `https://www.zhihu.com/people/${userId}/activities`;
+
+const firstUrl = `https://www.zhihu.com/api/v4/members/${userId}/activities?include=data%5B%3F(target.type%3Danswer)%5D.target.is_normal%2Csuggest_edit%2Ccontent%2Cvoteup_count%2Ccomment_count%2Ccollapsed_counts%2Creviewing_comments_count%2Ccan_comment%2Cmark_infos%2Ccreated_time%2Cupdated_time%2Crelationship.voting%2Cis_author%2Cis_thanked%2Cis_nothelp%2Cupvoted_followees%3Bdata%5B%3F(target.type%3Danswer)%5D.target.badge%5B%3F(type%3Dbest_answerer)%5D.topics%3Bdata%5B%3F(target.type%3Darticle)%5D.target.column%2Ccontent%2Cvoteup_count%2Ccomment_count%2Ccollapsed_counts%2Creviewing_comments_count%2Ccan_comment%2Ccomment_permission%2Ccreated%2Cupdated%2Cupvoted_followees%2Cvoting%2Cauthor.badge%5B%3F(type%3Dbest_answerer)%5D.topics%3Bdata%5B%3F(target.type%3Dcolumn)%5D.target.title%2Cintro%2Cdescription%2Carticles_count%2Cfollowers%3Bdata%5B%3F(target.type%3Dtopic)%5D.target.introduction%3Bdata%5B%3F(verb%3DMEMBER_COLLECT_ANSWER)%5D.extra_object%3Bdata%5B%3F(verb%3DMEMBER_COLLECT_ARTICLE)%5D.extra_object&limit=20`;
 
 // 授权
-const authorization = ''; 
+const authorization = 'Bearer Mi4wQUlDQ3J5bGNCUXNBWUFKOHQ0X3JDaGNBQUFCaEFsVk5EZFNDV0FBUFd0d0pseUpMYnRUTW8zTXFfWHB2UWEzNWJ3|1482377186|03bd216e3224470158e93217d56077e47a5092fc'; 
 // cookie
-const cookie = '';
-
-let firstId = '';
+const cookie = 'd_c0="AGACfLeP6wqPTjk6zoRY9xy0IjgZZx0opqE=|1480386949"; q_c1=f60013e56c084a24b0dd80ae793f7e76|1480386949000|1480386949000; _zap=fa71034a-810c-4829-9393-e27eeeedafdb; _xsrf=cfe19cb2d335d834399975f082005e21; l_n_c=1; l_cap_id="M2FjNDllMTQ3M2I2NGU0MWJkZjY5ZDQ1MmU0N2Q3MTI=|1482376701|a233e4b843ccd01d851d70efc238a6b31510706c"; cap_id="NzJhMDNjNDg1NzNkNGZjN2FkYTY4YTI3NjBlYjdkZmI=|1482376701|c23b16f0deb7f9fc8de6af302c141d009e50cb38"; r_cap_id="MzViMTg1Zjc4OGM5NDYzZWFkOTI1Y2UwM2ZmNDE3ZTI=|1482376703|be49b63b3e6b21e371199c3c472d9dddb9ece277"; login="ZTQ2NzA5MmE1MDA1NDgxMmE2NDMwYTFlOTRkNDE5YWI=|1482376754|32a8b6e27e68c5b1f93dfbb6155bba7281cffbc1"; z_c0=Mi4wQUlDQ3J5bGNCUXNBWUFKOHQ0X3JDaGNBQUFCaEFsVk5EZFNDV0FBUFd0d0pseUpMYnRUTW8zTXFfWHB2UWEzNWJ3|1482377186|03bd216e3224470158e93217d56077e47a5092fc';
 
 // 知乎渲染方式为首屏直出（前五条数据），下拉加载时每次请求新数据
 // 先抓取首屏页面，从中筛选出前面几条数据
@@ -63,8 +64,14 @@ const getProfile = (callback) => {
 // 抓取动态，每次20条
 const getActions = (callback) => {
   fetchAction();
-  function fetchAction (afterId = firstId) {
-  let targetUrl = `https://www.zhihu.com/api/v4/members/tiantang/activities?limit=${actionSize}&after_id=${afterId}&desktop=True`;
+  function fetchAction (afterId) {
+  let targetUrl;
+  if (afterId) {
+    targetUrl = `https://www.zhihu.com/api/v4/members/${userId}/activities?limit=${actionSize}&after_id=${afterId}&desktop=True`;
+  } else {
+    targetUrl = firstUrl;
+  }
+  
   console.log('抓取目标：' + targetUrl);
   superagent
   .get(targetUrl)
@@ -81,6 +88,12 @@ const getActions = (callback) => {
       actionArr.forEach((item) => actions.push(item));
       // 如果有不够 200 条则接着请求
       if (actions.length < actionCount) {
+        // 所有动态已请求完毕
+        if (result.paging.is_end) {
+          callback(actions);
+          return;
+        }
+
         console.log('才' + actions.length + ', 继续');
         let url = result.paging.next;
         let after_id = url.match(/after_id=(\d+)/)[1];
@@ -96,8 +109,7 @@ const getActions = (callback) => {
       callback(actions);
     }
   });
-  }
-  
+  } 
 }
 
 exports.getProfile = getProfile;
